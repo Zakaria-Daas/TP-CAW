0b9ce658cfc568ccd8a5a14d92ad5c5a499cf14d
Zakaria-Daas <daaszaki9@gmail.com>
2022-11-04 20:41:09 +0100

    Submit lab

--- TP5/search.js
+++ TP5/search.js
@@ -1,18 +1,53 @@
-// Make sure we got a filename on the command line.
+// Make sure we got file/directory names on the command line.
 if (process.argv.length < 4) {
-    console.log('Usage: node  ' + process.argv[1] + '  TEXT  FILENAMES');
+    console.log('Usage: node  ' + process.argv[1] + '  TEXT  FILE/DIRECTORY_NAMES');
     process.exit(1);
 }
-// Read the file and print its contents.
-var fs = require('fs'), textToSearch = process.argv[2];
+
+// Declare variables
+const glob = require("glob"), fs = require("fs"), textToSearch = process.argv[2];
+
+// Function to get files in a directory
+var getDirectories = function (src, callback) {
+    glob(src + '/**/*', callback);
+};
+
+// Read the files and print their contents.
 for (var i = 3; i < process.argv.length; i++) {
     let filename = process.argv[i];
-    fs.readFile(filename, 'utf8', function(err, data) {
-        if(err) {
-            return console.log(err);
-        }
-        if (data.includes(textToSearch)) {
-            console.log(filename);
-        }
-    });
-}
+    // If this argument provided on the command line is a directory
+    if (fs.lstatSync(filename).isDirectory()) {
+        // Note: the following function will not accept paths with \ since it's an escape character in js
+        getDirectories(filename, function (err, res) {
+            if (err) {
+                console.log('Error', err);
+            } else {
+                for (f of res) {
+                    const file = f;
+                    if (fs.lstatSync(f).isFile()) {
+                        fs.readFile(f, 'utf8', function(err, data) {
+                            if(err) {
+                                return console.log(err);
+                            }
+                            if (data.includes(textToSearch)) {
+                                console.log(file);
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+    // Else
+    else {
+        fs.readFile(filename, 'utf8', function(err, data) {
+            if(err) {
+                return console.log(err);
+            }
+            if (data.includes(textToSearch)) {
+                console.log(filename);
+            }
+        });
+    }   
+} 
+
